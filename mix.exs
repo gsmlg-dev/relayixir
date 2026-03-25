bd814230a4d2ceb9dcee90a2c9fc69a5684f9d29
Jonathan Gao <gsmlg.com@gmail.com>
2026-03-25 09:18:45 +0800

    ci(github): bump elixir version to 1.18

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Relayixir.MixProject do
     [
       app: :relayixir,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()
